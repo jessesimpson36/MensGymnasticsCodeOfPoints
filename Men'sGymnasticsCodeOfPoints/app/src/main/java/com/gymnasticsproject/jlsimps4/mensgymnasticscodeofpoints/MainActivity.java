324a8147353b50a45b6e283d64acf9a9ff33a58e
Jesse <jlsimps4@ncsu.edu>
2016-06-16 08:42:11 -0400

    the project now runs successfully with a few minor imperfections.

--- Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MainActivity.java
+++ Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MainActivity.java
@@ -15,89 +15,70 @@ import android.widget.Spinner;
  */
 public class MainActivity extends AppCompatActivity {
 
-    /**
-     * this is the spinner that displays the levels and points to a routine document
-     */
-    private Spinner routineSpinner;
 
     /**
-     * this is the spinner that displays the skill groups and points to the skill document
+     * does everything once the program starts.
+     * @param savedInstanceState auto-generated variable
      */
-    private Spinner skillSpinner;
-
-    /**
-     * the adapter for skills
-     */
-    private ArrayAdapter<CharSequence> skillAdapter;
-
-    /**
-     * the adapter for the levels
-     */
-    private ArrayAdapter<CharSequence> levelAdapter;
-
-    /**
-     * the skill button that points to a file
-     */
-    private Button skillButton;
-
-    /**
-     * the level button that points to the file
-     */
-    private Button routineButton;
-
-
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //finds the Ids for reference
-        routineSpinner = (Spinner) findViewById(R.id.routineSpinner);
-        skillSpinner = (Spinner) findViewById(R.id.skillSpinner);
-        routineButton = (Button) findViewById(R.id.routineButton);
-        skillButton = (Button) findViewById(R.id.skillButton);
+
+        /*
+      this is the spinner that displays the levels and points to a routine document
+     */
+        Spinner routineSpinner = (Spinner) findViewById(R.id.routineSpinner);
+
+        /*
+      this is the spinner that displays the skill groups and points to the skill document
+     */
+        Spinner skillSpinner = (Spinner) findViewById(R.id.skillSpinner);
+
+
+        /*
+      the level button that points to the file
+     */
+        Button routineButton = (Button) findViewById(R.id.routineButton);
+
+
+        /*
+      the skill button that points to a file
+     */
+        Button skillButton = (Button) findViewById(R.id.skillButton);
 
         //creates the reference and sets a dropdown resourse
-        skillAdapter = ArrayAdapter.createFromResource(this, R.array.skillGroups_array, android.R.layout.simple_spinner_item);
+
+
+        /*
+      the adapter for skills
+     */
+        ArrayAdapter<CharSequence> skillAdapter = ArrayAdapter.createFromResource(this, R.array.skillGroups_array, android.R.layout.simple_spinner_item);
         skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         //same thing for levels
-        levelAdapter = ArrayAdapter.createFromResource(this, R.array.levels_array, android.R.layout.simple_spinner_item);
+        /*
+      the adapter for the levels
+     */
+        ArrayAdapter<CharSequence> levelAdapter = ArrayAdapter.createFromResource(this, R.array.levels_array, android.R.layout.simple_spinner_item);
         levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         //sets the adapters
-        skillSpinner.setAdapter(skillAdapter);
-        routineSpinner.setAdapter(levelAdapter);
+//        skillSpinner.setAdapter(skillAdapter);
+//        routineSpinner.setAdapter(levelAdapter);
 
-        //for possible use
-//        skillSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//        routineButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            public void onClick(View view) {
 //
 //            }
 //        });
-
-//        routineSpinner.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+//
+//        skillButton.setOnClickListener(new View.OnClickListener() {
 //            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+//            public void onClick(View view) {
 //
 //            }
 //        });
-
-        routineButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
-
-        skillButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-            }
-        });
 
 
         super.onCreate(savedInstanceState);
--- Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MenuActivity.java
+++ Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MenuActivity.java
@@ -13,9 +13,35 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 
 public class MenuActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
+
+
+    /**
+     * this is the event that the user chooses and is regularly updated.
+     */
+    private static String event;
+
+    /**
+     * the adapter for skills
+     */
+    private ArrayAdapter<CharSequence> skillAdapter;
+
+    /**
+     * the adapter for the levels
+     */
+    private ArrayAdapter<CharSequence> levelAdapter;
+
+    /**
+     * gets the event that the user last chose.
+     * @return the event that the user last chose
+     */
+    public static String getEvent(){
+        return event;
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +67,16 @@ public class MenuActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //creates the reference and sets a dropdown resourse
+        skillAdapter = ArrayAdapter.createFromResource(this, R.array.skillGroups_array, android.R.layout.simple_spinner_item);
+        skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        //same thing for levels
+        levelAdapter = ArrayAdapter.createFromResource(this, R.array.levels_array, android.R.layout.simple_spinner_item);
+        levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+
     }
 
     @Override
@@ -87,16 +123,38 @@ public class MenuActivity extends AppCompatActivity
 
         if (id == R.id.rings) {
 
-        } else if (id == R.id.pommelhorse) {
+            //updates the text on the adapters
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.rings_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+            event = "rings";
+        } else if (id == R.id.pommelhorse) {
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.pommel_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            event = "pommelhorse";
         } else if (id == R.id.floor) {
 
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.floor_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            event = "floor";
         } else if (id == R.id.vault) {
 
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.vault_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            event = "vault";
         } else if (id == R.id.parallelbars) {
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.pbar_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+            event = "parallelbars";
         } else if (id == R.id.highbar) {
+            skillAdapter = ArrayAdapter.createFromResource(this, R.array.hbar_skillGroups_array, android.R.layout.simple_spinner_item);
+            skillAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+            event = "highbar";
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
