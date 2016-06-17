cbe8070c306edd316db14020be8857cb775e35ff
Jesse <jlsimps4@ncsu.edu>
2016-06-16 21:35:04 -0400

    made the navigation drawer visible

--- Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MainActivity.java
+++ Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MainActivity.java
@@ -1,14 +1,17 @@
 package com.gymnasticsproject.jlsimps4.mensgymnasticscodeofpoints;
 
 import android.net.Uri;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.Spinner;
-
 
 /**
  * this is the main activity
@@ -22,6 +25,9 @@ public class MainActivity extends AppCompatActivity {
      */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
 
 
         /*
@@ -81,8 +87,8 @@ public class MainActivity extends AppCompatActivity {
 //        });
 
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+
 
     }
 
--- Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MenuActivity.java
+++ Men'sGymnasticsCodeOfPoints/app/src/main/java/com/gymnasticsproject/jlsimps4/mensgymnasticscodeofpoints/MenuActivity.java
@@ -3,9 +3,8 @@ package com.gymnasticsproject.jlsimps4.mensgymnasticscodeofpoints;
 import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.Snackbar;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -13,6 +12,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 public class MenuActivity extends AppCompatActivity
@@ -50,14 +50,6 @@ public class MenuActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
@@ -75,7 +67,6 @@ public class MenuActivity extends AppCompatActivity
         //same thing for levels
         levelAdapter = ArrayAdapter.createFromResource(this, R.array.levels_array, android.R.layout.simple_spinner_item);
         levelAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
 
     }
 
@@ -119,7 +110,6 @@ public class MenuActivity extends AppCompatActivity
         int id = item.getItemId();
 
         FragmentManager fragmentManager = getFragmentManager();
-
 
         if (id == R.id.rings) {
 
